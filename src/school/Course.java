9d51d05553205acfed73ad4a15026149c32a6b55
Sarah Hendrickson <hendricksonsarahl@gmail.com>
2017-08-16 11:46:12 -0700

    Class 3.6 pre-work, implemented custom equals() methods for classes

--- src/school/Course.java
+++ src/school/Course.java
@@ -6,16 +6,59 @@ import java.util.HashMap;
 public class Course {
 
     private final int courseNumber;
-    private  int studentId;
-    private boolean isStudentEnrolled = true;
-    private ArrayList<HashMap<courseNumber, studentId>> allEnrolled;
+    private boolean whereStudentEnrolled = true;
+    private ArrayList<HashMap<Course, Student>> allEnrolled;
 
 
     public Course(int courseNumber, int studentId,
                    int numberOfCredits, double gpa) {
         this.courseNumber = courseNumber;
-        this.studentId = studentId;
-        this.isStudentEnrolled = isStudentEnrolled;
+        this.whereStudentEnrolled = whereStudentEnrolled;
+        this.allEnrolled = allEnrolled;
+    }
+
+    public boolean equals(Object o) {
+
+        if (o == this) {
+            return true;
+        }
+
+        if (o == null) {
+            return false;
+        }
+
+        if (o.getClass() != getClass()) {
+            return false;
+        }
+
+        Course theCourse = (Course) o;
+        return theCourse.getCourseNumber() == getCourseNumber();
+    }
+
+    /* getters and setters */
+
+    public int getCourseNumber() {
+        return courseNumber;
+    }
+
+    public static void setCourseNumber(int courseNumber) {
+        Course.courseNumber = courseNumber;
+    }
+
+    public boolean isWhereStudentEnrolled() {
+        return whereStudentEnrolled;
+    }
+
+    public void setWhereStudentEnrolled(boolean whereStudentEnrolled) {
+        this.whereStudentEnrolled = whereStudentEnrolled;
+    }
+
+    public ArrayList<HashMap<Course, Student>> getAllEnrolled() {
+        return allEnrolled;
+    }
+
+    public void setAllEnrolled(ArrayList<HashMap<Course, Student>> allEnrolled) {
         this.allEnrolled = allEnrolled;
     }
 }
+
--- src/school/Student.java
+++ src/school/Student.java
@@ -3,8 +3,8 @@ package school;
 public class Student {
 
     private static int nextStudentId = 1;
-    private String name;
     private final int studentId;
+    private String name;
     private int numberOfCredits;
     private double gpa;
 
@@ -25,8 +25,80 @@ public class Student {
         nextStudentId++;
     }
 
+    public boolean equals(Object o) {
+
+        if (o == this) {
+            return true;
+        }
+
+        if (o == null) {
+            return false;
+        }
+
+        if (o.getClass() != getClass()) {
+            return false;
+        }
+
+        Student theStudent = (Student) o;
+        return theStudent.getStudentId() == getStudentId();
+    }
+
+    public void addGrade(int courseCredits, double grade) {
+        // Update the appropriate fields: numberOfCredits, gpa
+        // GPA on 4.0 scale
+
+// Calculation to update numberOfCredits
+        if (grade < 60.0) courseCredits = 0; // grade = F
+        else if (grade >= 60.0 && grade < 70) courseCredits = 1; // grade = D (1 point)
+        else if (grade >= 70.0 && grade < 80) courseCredits = 2; // grade = C (2 points)
+        else if (grade >= 80.0 && grade < 90) courseCredits = 3; // grade = B (3 points)
+        else if (grade >= 90.0 && grade < 100) courseCredits = 4; // grade = A (4 points)
+
+        if (courseCredits >= 1) {
+            numberOfCredits = numberOfCredits + courseCredits;
+        }
+
+// TODO: figure out this quality score / GPA calculation stuff
+
+        qualityScore = gpa * numberOfCredits;
+        newQualityScore = gradePoints * courseCredits;
+        newGpa = qualtyScore / numberOfCredits;
+
+
+    }
+
+    public String getGradeLevel(String gradeLevel) {
+        // Determine the grade level of the student based on numberOfCredits
+        // For each passing class (D 60% or higher) student earns one credit/year
+        // Each year, students can earn up to 6 credits for a 4.0
+
+        gradeLevel = "";
+
+        // Under 6 = Freshman
+        if (numberOfCredits <= 29) gradeLevel = "Freshman";
+        // 6 - 12 = Sophomore
+        if (30 <= numberOfCredits && numberOfCredits <= 59) gradeLevel = "Sophomore";
+        // 12 - 18 = Junior
+        if (60 <= numberOfCredits && numberOfCredits <= 89) gradeLevel = "Junior";
+        // 18 - 24 = Senior
+        if (numberOfCredits >= 90) gradeLevel = "Senior";
+
+        return gradeLevel;
+
+
+    }
+
+
     /* getters and setters for fields: name, numberOfCredits, GPA. studentID
      * doesn't need a getter/setter as it is a final field. */
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public static void setStudentId(int studentId) {
+        Student.studentId = studentId;
+    }
 
     public String getName() {
         return name;
